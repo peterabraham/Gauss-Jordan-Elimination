24a52ae2bb06e13b8938d995df7aeaaeb17280b6
Peter <peterabraham@live.com>
2017-08-26 12:30:26 +0530

    modified

--- GaussJordanElimination.cpp
+++ GaussJordanElimination.cpp
@@ -6,8 +6,8 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-    
+int main()
+{
     int i = 0, j = 0, k = 0, n = 0;
     float **mat = NULL;
     float d = 0.0;
@@ -18,21 +18,26 @@ int main() {
     
     // Allocating memory for matrix array
     mat = new float*[2*n];
-    for (i = 0; i < 2*n; ++i) {
+    for (i = 0; i < 2*n; ++i)
+    {
         mat[i] = new float[2*n]();
     }
     
     cout << "Please enter the coefficients:" << endl;
     //Inputs the coefficients of the matrix
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < n; j++) {
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
             cin >> mat[i][j];
         }
     }
     
     cout << endl << "Input matrix:" << endl;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
             cout << mat[i][j] << "\t";
         }
         cout << endl;
@@ -40,18 +45,24 @@ int main() {
     cout << endl;
     
     // Initializing Right-hand side to identity matrix
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < 2*n; j++) {
-            if(j == (i+n)) {
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < 2*n; j++)
+        {
+            if(j == (i+n))
+            {
                 mat[i][j] = 1;
             }
         }
     }
     
     // Partial pivoting
-    for(i = n; i > 1; i--) {
-        if(mat[i-1][1] < mat[i][1]) {
-            for(j = 0; j < 2*n+1; j++) {
+    for(i = n; i > 1; i--)
+    {
+        if(mat[i-1][1] < mat[i][1])
+        {
+            for(j = 0; j < 2*n+1; j++)
+            {
                 d = mat[i][j];
                 mat[i][j] = mat[i-1][j];
                 mat[i-1][j] = d;
@@ -62,8 +73,10 @@ int main() {
     
     // Pivoted output
     cout << "Pivoted output: " << endl;
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < 2*n; j++) {
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < 2*n; j++)
+        {
             cout << mat[i][j] << "\t";
         }
         cout << endl;
@@ -71,11 +84,15 @@ int main() {
     cout << endl;
     
     // Reducing To Diagonal Matrix
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < 2*n; j++) {
-            if(j != i) {
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < 2*n; j++)
+        {
+            if(j != i)
+            {
                 d = mat[j][i] / mat[i][i];
-                for(k = 0; k < n*2; k++) {
+                for(k = 0; k < n*2; k++)
+                {
                     mat[j][k] -= mat[i][k]*d;
                 }
             }
@@ -84,24 +101,29 @@ int main() {
     cout << endl;
     
     // Reducing To Unit Matrix
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++)
+    {
         d = mat[i][i];
-        for(j = 0; j < 2*n; j++) {
+        for(j = 0; j < 2*n; j++)
+        {
             mat[i][j] = mat[i][j]/d;
         }
     }
     
     // Print inverse of the input matrix
     cout<<"Inverse matrix:" << endl;
-    for(i=0; i < n; i++) {
-        for(j = n; j < 2*n; j++) {
+    for(i=0; i < n; i++)
+    {
+        for(j = n; j < 2*n; j++)
+        {
             cout << mat[i][j] << "\t";
         }
         cout << endl;
     }
     
     // Deleting the memory allocated
-    for (i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i)
+    {
         delete[] mat[i];
     }
     delete[] mat;
